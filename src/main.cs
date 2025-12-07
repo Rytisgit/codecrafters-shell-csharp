bb973cd0eb0a458395268e582a348d922d3e6f41
Rytis Petronis <petronisrytis@gmail.com>
2025-12-07 11:46:08 +0100

    fix output loop

--- src/main.cs
+++ src/main.cs
@@ -5,11 +5,16 @@ namespace CodecraftersShell
         static void Main()
         {
             //TODO: Uncomment the code below to pass the first stage
-            Console.Write("$ ");
-            var command = Console.ReadLine();
+
+
             ConsoleManager manager = new ConsoleManager();
+            while (true)
+            {
+                Console.Write("$ ");
+                var command = Console.ReadLine();
+                Console.WriteLine(manager.CommandInput(command));
+            }
             
-            Console.WriteLine(manager.CommandInput(command));
         }
     }
 
